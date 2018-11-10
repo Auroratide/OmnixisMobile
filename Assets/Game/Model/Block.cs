8501d15dec004cd8bcd68d441aa5c3c10c1009b8
auroratide <tf.auroratide@gmail.com>
2018-11-10 21:13:43 +0800

    Remove unity connection to block model

--- Assets/Game/Model/Block.cs
+++ Assets/Game/Model/Block.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Auroratide.Omnixis.Model {
+﻿namespace Auroratide.Omnixis.Model {
   public class Block {
     private Position position;
     private Movement movement;
