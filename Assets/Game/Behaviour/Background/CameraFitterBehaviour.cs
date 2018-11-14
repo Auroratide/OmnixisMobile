d2f85602754fe1381927883dba7a31670d9248bf
auroratide <tf.auroratide@gmail.com>
2018-11-14 08:54:46 +0800

    Script for fitting the camera to a particular object, in this case the background

---
+++ Assets/Game/Behaviour/Background/CameraFitterBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Auroratide.Omnixis.Behaviour {
+
+  // https://answers.unity.com/questions/1231701/fitting-bounds-into-orthographic-2d-camera.html
+  [RequireComponent(typeof(Camera))]
+  public class CameraFitterBehaviour : MonoBehaviour {
+
+    [SerializeField] private Config config;
+    private Camera camera;
+    private Bounds bounds;
+
+    public void Start() {
+      camera = GetComponent<Camera>();
+      bounds = config.fitTo.bounds;
+    }
+
+    public void LateUpdate() {
+      float screenRatio = (float)Screen.width / (float)Screen.height;
+      float targetRatio = bounds.size.x / bounds.size.y;
+
+      if(screenRatio > targetRatio) {
+        camera.orthographicSize = bounds.size.y / 2;
+      } else {
+        float difference = targetRatio / screenRatio;
+        camera.orthographicSize = bounds.size.y / 2 * difference;
+      }
+
+      transform.position = new Vector3(bounds.center.x, bounds.center.y, transform.position.z);
+    }
+
+    [System.Serializable] public class Config {
+      public Renderer fitTo;
+    }
+  }
+}
